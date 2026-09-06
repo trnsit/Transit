aef4d50c2ae75592b336b465d524059224d64372
hisham Ali <thehishamali@gmail.com>
2026-09-07 00:07:17 +0530

    fix: correct app version and debug settings

--- backend/services/repositories/main.py
+++ backend/services/repositories/main.py
@@ -6,8 +6,8 @@ from .routes import router as repository_router
 
 app = FastAPI(
     title='Transit - Repositories Service',
-    version='1.0.0.',
-    debug=settings
+    version='1.0.0',
+    debug=settings.debug
 )
 
 app.include_router(repository_router)
